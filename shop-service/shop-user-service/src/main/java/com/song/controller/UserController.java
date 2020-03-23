95950aa55e1f335e6f0b7db7b1f7213255b74fc5
炜哥哥 <https://gitee.com/studayOnQF/FengMiNet.git>
2020-03-23 17:35:42 +0800

    实现登录和邮箱注册,添加返回给客户端cookie 存的是redis中存用户信息的键

--- shop-service/shop-user-service/src/main/java/com/song/controller/UserController.java
+++ shop-service/shop-user-service/src/main/java/com/song/controller/UserController.java
@@ -5,10 +5,14 @@ import com.song.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import resoutBean.ResultBean;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author 炜哥哥
@@ -23,10 +27,23 @@ public class UserController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    //用户登录 将登录的信息存入redis 并且 返回一个cookie给客户端 cookie存的就是redis中的键
     @RequestMapping("/checkLogin")
-    public ResultBean login(String phone, String password) {
+    public String login(String phone, String password, HttpServletResponse response) {
         TUserDTO userById = userService.findUserById(phone, password);
-        return new ResultBean(200, "success", userById);
+        if (userById != null) {
+            //组织cookie的键  返回给客户端保存
+            String cookie_key = String.format("user_info:%s", userById.getPhone());
+            Cookie cookie = new Cookie("cookie_user_login_key", cookie_key);
+            cookie.setPath("/");
+            cookie.setMaxAge(30 * 24 * 60 * 60);
+            cookie.setHttpOnly(true);
+            response.addCookie(cookie);
+            //return到具体的页面  return的是一个页面 如；登录成功的页面
+            return "index";
+        } else {
+            return "index";
+        }
     }
 
     //用户注册
